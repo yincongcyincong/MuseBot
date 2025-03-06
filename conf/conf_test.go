f95396fa8272d45234b93fe0bd496356f32780e8
yincong <yincong@baidu.com>
2025-03-06 11:49:01 +0800

    update unit test

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -13,6 +13,8 @@ func TestInitConf(t *testing.T) {
 	os.Setenv("DEEPSEEK_TOKEN", "test_deepseek_token")
 	os.Setenv("CUSTOM_URL", "https://test.deepseek.com/")
 	os.Setenv("DEEPSEEK_TYPE", "test_type")
+	os.Setenv("VOLC_AK", "ak")
+	os.Setenv("VOLC_SK", "sk")
 
 	// call InitConf
 	InitConf()
@@ -22,4 +24,7 @@ func TestInitConf(t *testing.T) {
 	assert.Equal(t, "test_deepseek_token", *DeepseekToken, "DeepseekToken should be from environment variable")
 	assert.Equal(t, "https://test.deepseek.com/", *CustomUrl, "CustomUrl should be from environment variable")
 	assert.Equal(t, "test_type", *DeepseekType, "DeepseekType should be from environment variable")
+	assert.Equal(t, "ak", *VolcAK, "VolcAK should be from environment variable")
+	assert.Equal(t, "sk", *VolcSK, "VolcSK should be from environment variable")
+
 }
