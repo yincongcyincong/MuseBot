ac7f700705e661811e239f8de687227b13c06b0a
yincong <yincong@baidu.com>
2025-04-22 16:50:50 +0800

    add new mcp server

--- conf/tools_conf.go
+++ conf/tools_conf.go
@@ -1,147 +1,240 @@
 package conf
 
 import (
-	"context"
-	"flag"
-	"github.com/yincongcyincong/mcp-client-go/clients/victoriametrics"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/cohesion-org/deepseek-go"
-	"github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
-	"github.com/yincongcyincong/mcp-client-go/clients"
-	"github.com/yincongcyincong/mcp-client-go/clients/amap"
-	"github.com/yincongcyincong/mcp-client-go/clients/github"
-	"github.com/yincongcyincong/mcp-client-go/clients/param"
-	"github.com/yincongcyincong/mcp-client-go/utils"
-	"github.com/yincongcyincong/telegram-deepseek-bot/logger"
+    "context"
+    "flag"
+    "github.com/cohesion-org/deepseek-go"
+    "github.com/volcengine/volcengine-go-sdk/service/arkruntime/model"
+    "github.com/yincongcyincong/mcp-client-go/clients"
+    "github.com/yincongcyincong/mcp-client-go/clients/amap"
+    "github.com/yincongcyincong/mcp-client-go/clients/binance"
+    "github.com/yincongcyincong/mcp-client-go/clients/filesystem"
+    "github.com/yincongcyincong/mcp-client-go/clients/github"
+    "github.com/yincongcyincong/mcp-client-go/clients/param"
+    "github.com/yincongcyincong/mcp-client-go/clients/playwright"
+    mcp_time "github.com/yincongcyincong/mcp-client-go/clients/time"
+    "github.com/yincongcyincong/mcp-client-go/clients/victoriametrics"
+    "github.com/yincongcyincong/mcp-client-go/utils"
+    "github.com/yincongcyincong/telegram-deepseek-bot/logger"
+    "os"
+    "strings"
 )
 
 var (
-	AmapApiKey        *string
-	GithubAccessToken *string
-	VMUrl             *string
-	VMInsertUrl       *string
-	VMSelectUrl       *string
-	AllTools          *string
+    AmapApiKey          *string
+    GithubAccessToken   *string
+    VMUrl               *string
+    VMInsertUrl         *string
+    VMSelectUrl         *string
+    BinanceSwitch       *bool
+    TimeZone            *string
+    PlayWrightSSEServer *string
+    PlayWrightSwitch    *bool
+    FilecrawlApiKey     *string
+    FilePath            *string
 
-	DeepseekTools = make([]deepseek.Tool, 0)
-	VolTools      = make([]*model.Tool, 0)
+    AllTools *string
+
+    DeepseekTools = make([]deepseek.Tool, 0)
+    VolTools      = make([]*model.Tool, 0)
 )
 
 func InitToolsConf() {
-	AmapApiKey = flag.String("amap_api_key", "", "amap api key")
-	AllTools = flag.String("allow_tools", "*", "allow tools")
-	GithubAccessToken = flag.String("github_access_token", "", "github access token")
-	VMUrl = flag.String("vm_url", "", "vm url")
-	VMInsertUrl = flag.String("vm_insert_url", "", "vm insert url")
-	VMSelectUrl = flag.String("vm_select_url", "", "vm select url")
+    AmapApiKey = flag.String("amap_api_key", "", "amap api key")
+    AllTools = flag.String("allow_tools", "*", "allow tools")
+    GithubAccessToken = flag.String("github_access_token", "", "github access token")
+    VMUrl = flag.String("vm_url", "", "vm url")
+    VMInsertUrl = flag.String("vm_insert_url", "", "vm insert url")
+    VMSelectUrl = flag.String("vm_select_url", "", "vm select url")
+    BinanceSwitch = flag.Bool("binance_switch", false, "binance switch")
+    TimeZone = flag.String("time_zone", "", "time zone")
+    PlayWrightSwitch = flag.Bool("play_wright_switch", false, "playwright switch")
+    PlayWrightSSEServer = flag.String("play_wright_sse_server", "", "playwright sw server")
+    FilecrawlApiKey = flag.String("filecrawl_api_key", "", "filecrawl_api_key")
+    FilePath = flag.String("file_path", "", "file path")
 
-	if os.Getenv("AMAP_API_KEY") != "" {
-		*AmapApiKey = os.Getenv("AMAP_API_KEY")
-	}
+    if os.Getenv("AMAP_API_KEY") != "" {
+        *AmapApiKey = os.Getenv("AMAP_API_KEY")
+    }
 
-	if os.Getenv("ALLOW_TOOLS") != "" {
-		*AllTools = os.Getenv("ALLOW_TOOLS")
-	}
+    if os.Getenv("ALLOW_TOOLS") != "" {
+        *AllTools = os.Getenv("ALLOW_TOOLS")
+    }
 
-	if os.Getenv("GITHUB_ACCESS_TOKEN") != "" {
-		*GithubAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
-	}
+    if os.Getenv("GITHUB_ACCESS_TOKEN") != "" {
+        *GithubAccessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
+    }
 
-	if os.Getenv("VM_URL") != "" {
-		*VMUrl = os.Getenv("VM_URL")
-	}
+    if os.Getenv("VM_URL") != "" {
+        *VMUrl = os.Getenv("VM_URL")
+    }
 
-	if os.Getenv("VM_INSERT_URL") != "" {
-		*VMInsertUrl = os.Getenv("VM_INSERT_URL")
-	}
+    if os.Getenv("VM_INSERT_URL") != "" {
+        *VMInsertUrl = os.Getenv("VM_INSERT_URL")
+    }
 
-	if os.Getenv("VM_SELECT_URL") != "" {
-		*VMSelectUrl = os.Getenv("VM_SELECT_URL")
-	}
+    if os.Getenv("VM_SELECT_URL") != "" {
+        *VMSelectUrl = os.Getenv("VM_SELECT_URL")
+    }
 
-	logger.Info("TOOLS_CONF", "AmapApiKey", *AmapApiKey)
-	logger.Info("TOOLS_CONF", "AmapTools", *AllTools)
-	logger.Info("TOOLS_CONF", "GithubAccessToken", *GithubAccessToken)
+    if os.Getenv("BINANCE_SWITCH") != "" {
+        *BinanceSwitch = true
+    }
+
+    if os.Getenv("TIME_ZONE") != "" {
+        *TimeZone = os.Getenv("TIME_ZONE")
+    }
+
+    if os.Getenv("PLAY_WRIGHT_SSE_SERVER") != "" {
+        *PlayWrightSSEServer = os.Getenv("PLAY_WRIGHT_SSE_SERVER")
+    }
+
+    if os.Getenv("PLAY_WRIGHT_SWITCH") != "" {
+        *PlayWrightSwitch = true
+    }
+
+    if os.Getenv("FILECRAWL_API_KEY") != "" {
+        *FilecrawlApiKey = os.Getenv("FILECRAWL_API_KEY")
+    }
+
+    if os.Getenv("FILE_PATH") != "" {
+        *FilePath = os.Getenv("FILE_PATH")
+    }
+
+    logger.Info("TOOLS_CONF", "AmapApiKey", *AmapApiKey)
+    logger.Info("TOOLS_CONF", "AmapTools", *AllTools)
+    logger.Info("TOOLS_CONF", "GithubAccessToken", *GithubAccessToken)
+    logger.Info("TOOLS_CONF", "VMUrl", *VMUrl)
+    logger.Info("TOOLS_CONF", "VMInsertUrl", *VMInsertUrl)
+    logger.Info("TOOLS_CONF", "VMSelectUrl", *VMSelectUrl)
+    logger.Info("TOOLS_CONF", "BinanceSwitch", *BinanceSwitch)
+    logger.Info("TOOLS_CONF", "TimeZone", *TimeZone)
+    logger.Info("TOOLS_CONF", "PlayWrightSwitch", *PlayWrightSwitch)
+    logger.Info("TOOLS_CONF", "PlayWrightSSEServer", *PlayWrightSSEServer)
+    logger.Info("TOOLS_CONF", "FilePath", *FilePath)
 
 }
 
 func InitTools() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	defer cancel()
+    ctx := context.Background()
 
-	allTools := make(map[string]bool)
-	for _, tool := range strings.Split(*AllTools, ",") {
-		allTools[tool] = true
-	}
+    allTools := make(map[string]bool)
+    for _, tool := range strings.Split(*AllTools, ",") {
+        allTools[tool] = true
+    }
 
-	mcpParams := make([]*param.MCPClientConf, 0)
-	if *AmapApiKey != "" {
-		mcpParams = append(mcpParams,
-			amap.InitAmapMCPClient(&amap.AmapParam{
-				AmapApiKey: *AmapApiKey,
-			}, "", nil, nil, nil))
-	}
+    mcpParams := make([]*param.MCPClientConf, 0)
+    if *AmapApiKey != "" {
+        mcpParams = append(mcpParams,
+            amap.InitAmapMCPClient(&amap.AmapParam{
+                AmapApiKey: *AmapApiKey,
+            }, "", nil, nil, nil))
+    }
 
-	if *GithubAccessToken != "" {
-		mcpParams = append(mcpParams,
-			github.InitModelContextProtocolGithubMCPClient(&github.GithubParam{
-				GithubAccessToken: *GithubAccessToken,
-			}, "", nil, nil, nil))
-	}
+    if *GithubAccessToken != "" {
+        mcpParams = append(mcpParams,
+            github.InitModelContextProtocolGithubMCPClient(&github.GithubParam{
+                GithubAccessToken: *GithubAccessToken,
+            }, "", nil, nil, nil))
+    }
 
-	if *VMUrl != "" || *VMInsertUrl != "" || *VMSelectUrl != "" {
-		mcpParams = append(mcpParams, victoriametrics.InitVictoriaMetricsMCPClient(&victoriametrics.VictoriaMetricsParam{
-			VMUrl:       *VMUrl,
-			VMInsertUrl: *VMInsertUrl,
-			VMSelectUrl: *VMSelectUrl,
-		}, "", nil, nil, nil))
-	}
+    if *VMUrl != "" || *VMInsertUrl != "" || *VMSelectUrl != "" {
+        mcpParams = append(mcpParams, victoriametrics.InitVictoriaMetricsMCPClient(&victoriametrics.VictoriaMetricsParam{
+            VMUrl:       *VMUrl,
+            VMInsertUrl: *VMInsertUrl,
+            VMSelectUrl: *VMSelectUrl,
+        }, "", nil, nil, nil))
+    }
 
-	err := clients.RegisterMCPClient(ctx, mcpParams)
-	if len(err) > 0 {
-		logger.Error("register mcp client error", "errors", err)
-	}
+    if *BinanceSwitch {
+        mcpParams = append(mcpParams, binance.InitBinanceMCPClient(&binance.BinanceParam{},
+            "", nil, nil, nil))
+    }
 
-	if *AmapApiKey != "" {
-		InsertTools(amap.NpxAmapMapsMcpServer, allTools)
-	}
+    if *TimeZone != "" {
+        mcpParams = append(mcpParams, mcp_time.InitTimeMCPClient(&mcp_time.TimeParma{
+            LocalTimezone: *TimeZone,
+        }, "", nil, nil, nil))
+    }
 
-	if *GithubAccessToken != "" {
-		InsertTools(github.NpxModelContextProtocolGithubServer, allTools)
-	}
+    if *PlayWrightSwitch {
+        if *PlayWrightSSEServer != "" {
+            mcpParams = append(mcpParams, playwright.InitPlaywrightSSEMCPClient(*PlayWrightSSEServer,
+                nil, "", nil, nil, nil))
+        } else {
+            mcpParams = append(mcpParams, playwright.InitPlaywrightMCPClient(&playwright.PlaywrightParam{},
+                "", nil, nil, nil))
+        }
+    }
 
-	if *VMUrl != "" || *VMInsertUrl != "" || *VMSelectUrl != "" {
-		InsertTools(victoriametrics.NpxVictoriaMetricsMcpServer, allTools)
-	}
+    if *FilePath != "" {
+        mcpParams = append(mcpParams, filesystem.InitFilesystemMCPClient(&filesystem.FilesystemParam{
+            Paths: strings.Split(*FilePath, ","),
+        }, "", nil, nil, nil))
+    }
+
+    err := clients.RegisterMCPClient(ctx, mcpParams)
+    if len(err) > 0 {
+        logger.Error("register mcp client error", "errors", err)
+    }
+
+    if *AmapApiKey != "" {
+        InsertTools(amap.NpxAmapMapsMcpServer, allTools)
+    }
+
+    if *GithubAccessToken != "" {
+        InsertTools(github.NpxModelContextProtocolGithubServer, allTools)
+    }
+
+    if *VMUrl != "" || *VMInsertUrl != "" || *VMSelectUrl != "" {
+        InsertTools(victoriametrics.NpxVictoriaMetricsMcpServer, allTools)
+    }
+
+    if *BinanceSwitch {
+        InsertTools(binance.NpxBinanceMcpServer, allTools)
+    }
+
+    if *TimeZone != "" {
+        InsertTools(mcp_time.UvTimeMcpServer, allTools)
+    }
+
+    if *PlayWrightSwitch {
+        if *PlayWrightSSEServer != "" {
+            InsertTools(playwright.SsePlaywrightMcpServer, allTools)
+        } else {
+            InsertTools(playwright.NpxPlaywrightMcpServer, allTools)
+        }
+    }
+
+    if *FilePath != "" {
+        InsertTools(filesystem.NpxFilesystemMcpServer, allTools)
+    }
 
 }
 
 func InsertTools(clientName string, allTools map[string]bool) {
-	c, err := clients.GetMCPClient(clientName)
-	if err != nil {
-		logger.Error("get client fail", "err", err)
-	} else {
-		dpTools := utils.TransToolsToDPFunctionCall(c.Tools)
-		volTools := utils.TransToolsToVolFunctionCall(c.Tools)
+    c, err := clients.GetMCPClient(clientName)
+    if err != nil {
+        logger.Error("get client fail", "err", err)
+    } else {
+        dpTools := utils.TransToolsToDPFunctionCall(c.Tools)
+        volTools := utils.TransToolsToVolFunctionCall(c.Tools)
 
-		if allTools["*"] {
-			DeepseekTools = append(DeepseekTools, dpTools...)
-			VolTools = append(VolTools, volTools...)
-		} else {
-			for _, dpTool := range dpTools {
-				if _, ok := allTools[dpTool.Function.Name]; ok {
-					DeepseekTools = append(DeepseekTools, dpTool)
-				}
-			}
+        if allTools["*"] {
+            DeepseekTools = append(DeepseekTools, dpTools...)
+            VolTools = append(VolTools, volTools...)
+        } else {
+            for _, dpTool := range dpTools {
+                if _, ok := allTools[dpTool.Function.Name]; ok {
+                    DeepseekTools = append(DeepseekTools, dpTool)
+                }
+            }
 
-			for _, volTool := range volTools {
-				if _, ok := allTools[volTool.Function.Name]; ok {
-					VolTools = append(VolTools, volTool)
-				}
-			}
-		}
-	}
+            for _, volTool := range volTools {
+                if _, ok := allTools[volTool.Function.Name]; ok {
+                    VolTools = append(VolTools, volTool)
+                }
+            }
+        }
+    }
 }
--- static/doc/functioncall.md
+++ static/doc/functioncall.md
@@ -1,26 +1,59 @@
 ## Function Call Specification
 
-The telegram-deepseek-bot integrates with the MCP client via [https://github.com/yincongcyincong/mcp-client-go](https://github.com/yincongcyincong/mcp-client-go) to automatically request MCP server data and interact with DeepSeek.
+The telegram-deepseek-bot integrates with the MCP client
+via [https://github.com/yincongcyincong/mcp-client-go](https://github.com/yincongcyincong/mcp-client-go) to
+automatically request MCP server data and interact with DeepSeek.
 
 ### Supported Services
 
-| MCP Client         | Description               | Environment Variables                                                                 |
-|--------------------|---------------------------|--------------------------------------------------------------------------------------|
-| AMAP               | AMAP MCP Service          | AMAP_API_KEY: AMAP access token                                                      |
-| GitHub             | GitHub MCP Service        | GITHUB_ACCESS_TOKEN: GitHub access token                                             |
-| Victoria Metrics   | VM Metrics Service        | VMUrl: Single-node VM URL, VMInsertUrl: Cluster write URL, VMSelectUrl: Cluster query URL |
+| MCP Client       | Description             | Environment Variables                                                                     |
+|------------------|-------------------------|-------------------------------------------------------------------------------------------|
+| AMAP             | AMAP MCP Service        | AMAP_API_KEY: AMAP access token                                                           |
+| GitHub           | GitHub MCP Service      | GITHUB_ACCESS_TOKEN: GitHub access token                                                  |
+| Victoria Metrics | VM Metrics Service      | VMUrl: Single-node VM URL, VMInsertUrl: Cluster write URL, VMSelectUrl: Cluster query URL |
+| Time             | Time MCP Service        | TIME_ZONE: local time zone                                                                |
+| Binance          | Binance MCP Service     | BINANCE_SWITCH                                                                            |
+| Play Wright      | Play Wright MCP Service | PLAY_WRIGHT_SWITCH                                                                        |
+| File System      | File MCP Service        | FILE_PATH: multi computer directory, split by ','                                         |
+| FILE CRAWL       | FILECRAWL MCP Service   | FILECRAWL_API_KEY                                                                         |
+
 
 ### Usage Instructions
 
 1. **AMAP Service**:
     - Requires configuring the `AMAP_API_KEY` environment variable
-    - Provides geocoding, reverse geocoding and other location-based services
+    - Provides geocoding, reverse geocoding, IP location, and route planning services
 
 2. **GitHub Service**:
     - Requires configuring the `GITHUB_ACCESS_TOKEN` environment variable
-    - Supports repository information, user data queries and other features
+    - Supports repository information, user profile queries, commit history, and organization data
 
 3. **Victoria Metrics Service**:
     - Supports both single-node and cluster modes
-    - Single-node mode only requires `VMUrl` configuration
+    - Single-node mode requires `VMUrl` configuration
     - Cluster mode requires separate configuration of write (`VMInsertUrl`) and query (`VMSelectUrl`) URLs
+
+4. **Time Service**:
+    - Requires setting `TIME_ZONE` environment variable (e.g., `Asia/Shanghai`, `UTC`)
+    - Returns the current local time based on the configured timezone
+    - Useful for time-based automation and context-aware responses
+
+5. **Binance Service**:
+    - Requires enabling `BINANCE_SWITCH` (e.g., `true`)
+    - Fetches real-time cryptocurrency data, including prices, tickers, and volume
+    - Supports querying by symbol, e.g., BTC, ETH, etc.
+
+6. **Play Wright Service**:
+    - Requires enabling `PLAY_WRIGHT_SWITCH` (e.g., `true`)
+    - Allows automated browser interactions such as scraping, screenshots, or headless browsing
+    - Suitable for automating interaction with web content not accessible via API
+
+7. **File System Service**:
+    - Requires setting `FILE_PATH` environment variable with comma-separated paths (e.g., `/mnt/data1,/mnt/data2`)
+    - Enables reading from local or network-mounted directories
+    - Supports searching, reading, or listing files for multi-device setups
+
+8. **FILE CRAWL Service**:
+    - Requires setting `FILECRAWL_API_KEY`
+    - Enables crawling and indexing of files from given paths or URLs
+    - Useful for building searchable file databases or document retrieval systems
