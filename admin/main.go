5835e6d262e90e3f96272c233364a6048d10b6bf
yincong <yincong@baidu.com>
2025-07-01 18:59:50 +0800

    add admin

--- admin/main.go
+++ admin/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"embed"
+	"fmt"
+	"net/http"
+)
+
+//go:embed adminui/*
+var staticFiles embed.FS
+
+func main() {
+	fs := http.FileServer(http.FS(staticFiles))
+	http.Handle("/", fs)
+	http.HandleFunc("/hello", helloHandler)
+	
+	err := http.ListenAndServe(":18080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
